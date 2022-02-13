1c0586e68c10769dddace414f06e4fe3bff07ebb
Gabriel Ayala <gabeayala100@gmail.com>
2022-02-13 12:43:33 -0800

    Apply middleware to the redux store so it may handle async api calls.

--- src/index.js
+++ src/index.js
@@ -2,12 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
-import { createStore} from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import { ThunkMiddleware } from 'redux-thunk';
+import middlewareLogger from './middleware/middleware-logger';
 import reducer from './reducers/headlines-reducer';
 import { Provider } from 'react-redux';
+
 import reportWebVitals from './reportWebVitals';
 
-const store = createStore(reducer);
+const store = createStore(reducer, applyMiddleware(thunkMiddleware, middlewareLogger));
 
 ReactDOM.render(
   <Provider store={store}>
