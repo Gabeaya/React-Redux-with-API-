b87fa9eac4af10671f6f83c75db9d1fcb085f5c5
Gabriel Ayala <gabeayala100@gmail.com>
2022-02-13 11:53:27 -0800

    Add reducers for requesting and also recieving headlines.

--- src/reducers/headlines-reducer.js
+++ src/reducers/headlines-reducer.js
@@ -1,9 +1,23 @@
-const defaultState = {
+import * as c from './../actions/ActionTypes';
+
+const initialState = {
   isLoading: false,
   headlines: [],
   error: null
 }
 
-export default (state = defaultState, action) => {
-  return state;
+export default (state = initialState, action) => {
+  switch (action.type) {
+    case c.REQUEST_HEADLINES:
+      return Object.assign({}, state, {
+        isLoading: true
+      });
+    case c.GET_HEADLINES_SUCCESS:
+      return Object.assign({}, state, {
+        isLoading: false,
+        headlines: action.headlines
+      });
+    default:
+      return state;
+    }
 };
