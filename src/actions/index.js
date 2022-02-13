63a6d51b56f2c8fa7347e1c499a0a4c47c7fca41
Gabriel Ayala <gabeayala100@gmail.com>
2022-02-13 13:06:32 -0800

    Add the api call method to index of actions .

--- src/actions/index.js
+++ src/actions/index.js
@@ -13,3 +13,18 @@ export const getHeadlinesFailure = (error) => ({
   type: c.GET_HEADLINES_FAILURE,
   error
 });
+
+export const makeApiCall = () => {
+  return dispatch => { //instead of updating local state we are dispatching 
+    dispatch(requestHeadlines);//actions like requestheadlines to the redux store, these acrtions will update the store
+    return fetch (`https://api.nytimes.com/svc/topstories/v2/home.json?api-key=${process.env.REACT_APP_API_KEY}`)
+    .then(response => response.json())
+    .then(
+      (jsonifiedResponse) => {
+        dispatch(getHeadlinesSuccess(jsonifiedResponse.results));
+      })
+      .catch((error) => {
+        dispatch(getHeadlinesFailer(error));
+      });
+  }
+}
