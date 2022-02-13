5588f9632fea100376385378a75873c2c41bd6dc
Gabriel Ayala <gabeayala100@gmail.com>
2022-02-13 13:56:19 -0800

    Fix spelling.

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './components/App';
+import App from './App';
 import { createStore, applyMiddleware } from 'redux';
-import { ThunkMiddleware } from 'redux-thunk';
+import thunkMiddleware from 'redux-thunk';
 import middlewareLogger from './middleware/middleware-logger';
 import reducer from './reducers/headlines-reducer';
 import { Provider } from 'react-redux';
 
 import reportWebVitals from './reportWebVitals';
 
-const store = createStore(reducer, applyMiddleware(thunkMiddleware, middlewareLogger));
+const store = createStore(reducer, applyMiddleware(middlewareLogger, thunkMiddleware));
 
 ReactDOM.render(
   <Provider store={store}>
